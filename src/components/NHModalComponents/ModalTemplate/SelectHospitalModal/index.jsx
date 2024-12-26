b19d3c3b13f85be3b8ed673135afa55b6e694670
Trupesh Savaliya <trupesh.savaliya@swiftrut.com>
2024-12-27 04:52:43 +0530

    Update index.jsx
    bug fix

--- src/components/NHModalComponents/ModalTemplate/SelectHospitalModal/index.jsx
+++ src/components/NHModalComponents/ModalTemplate/SelectHospitalModal/index.jsx
@@ -13,7 +13,7 @@ export const SelectHospitalModal = ({ open, onCancel, handleClose }) => {
 
   return (
     <NHModal
-      title="Hospital Name ss"
+      title="Hospital Name"
       IsFooter
       open={open}
       onCancel={onCancel}
