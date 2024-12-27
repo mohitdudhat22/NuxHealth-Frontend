c9f0564a9b5d054a596b0e73a72be5b028dc8f16
Trupesh Savaliya <trupesh.savaliya@swiftrut.com>
2024-12-28 01:44:12 +0530

    Add Babel and ESLint configuration; implement doctor creation hook and API integration

--- src/hook/Admin/index.js
+++ src/hook/Admin/index.js
@@ -1,2 +1,4 @@
 export { useDoctorManagement } from "./DoctorManagement/"
+export { useCreateDoctor } from "./DoctorManagement/Create/"
+
 export { useReceptionManagement } from "./ReceptionManagement/"
