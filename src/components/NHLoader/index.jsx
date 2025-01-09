d19a90aaa994434ccba744f56dca8cee55c6fe39
jigarmakwana <jigarmakwana34065@gmail.com>
2025-01-10 01:06:29 +0530

    component css fix

--- src/components/NHLoader/index.jsx
+++ src/components/NHLoader/index.jsx
@@ -4,11 +4,30 @@ import styles from "./NHLoader.module.css";
 export const NHLoader = () => {
   return (
     <>
-      <div className={clsx(styles.body)}>
-        <div className={clsx(styles.loader)}>
-          <svg className={styles.circular} viewBox="25 25 50 50">
+      <div
+        className={clsx(
+          styles.body,
+          "h-[100dvh] grid place-content-center overflow-hidden antialiased"
+        )}
+      >
+        <div
+          className={clsx(
+            styles.loader,
+            "relative m-auto w-20 aspect-[1/1] rounded-[100vmin] overflow-hidden p-5 before:content-[''] before:block before:pt-[100%]"
+          )}
+        >
+          <svg
+            className={
+              (styles.circular,
+              "w-full h-full absolute inset-0 m-auto origin-center animate-[rotate_2s_linear_infinite]")
+            }
+            viewBox="25 25 50 50"
+          >
             <circle
-              className={styles.path}
+              className={
+                (styles.path,
+                "stroke-primary [stroke-dasharray:1,200] [stroke-dashoffset:0] stroke-round animate-[dash_1.5s_ease-in-out_infinite]")
+              }
               cx="50"
               cy="50"
               r="20"
