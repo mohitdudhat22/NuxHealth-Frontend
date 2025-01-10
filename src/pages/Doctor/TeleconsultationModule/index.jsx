1cfbffa529cb3974c9ee3e8cfa95c27664f12598
asavant151 <asavant151@gmail.com>
2025-01-11 04:20:59 +0530

    Refactor chat components for improved styling and responsiveness; enhance notification box styles and adjust button layouts in teleconsultation module.

--- src/pages/Doctor/TeleconsultationModule/index.jsx
+++ src/pages/Doctor/TeleconsultationModule/index.jsx
@@ -145,10 +145,10 @@ export const Teleconsultation = () => {
                     appointmentDate={appointmentDate || date}
                     appointmentTime={appointmentTime}
                     footerContent={
-                      <div className="flex justify-between gap-4">
+                      <div className="flex justify-between gap-4 w-full">
                         <NHButton
                           size={"small"}
-                          className={"w-full"}
+                          className={"w-full py-3 px-4"}
                           onClick={() => handleJoinCall(data)}
                         >
                           Join Call
@@ -156,7 +156,7 @@ export const Teleconsultation = () => {
                         <NHButton
                           size={"small"}
                           icon={Icons.CalenderIcon}
-                          className={"w-full"}
+                          className={"w-full py-3 px-4"}
                           onClick={() => handelReschedule()}
                         >
                           Reschedule
