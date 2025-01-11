91a4e66572d20f07116c6b22718af799fed65178
Trupesh Savaliya <trupesh.savaliya@swiftrut.com>
2025-01-11 23:18:10 +0530

    Refactor ReceptionManagement and Modal components; enhance NHButton for cancel and reschedule actions; clean up AppointmentBooking page; improve NHTable and PatientRecordAccess layout

--- src/pages/Doctor/PatientRecordAccess/index.jsx
+++ src/pages/Doctor/PatientRecordAccess/index.jsx
@@ -1,34 +1,37 @@
-import { NHCard, NHInput, NHSelect, NHTable } from "@/components"
-import Icons from "@/constants/icons"
+import { NHCard, NHInput, NHSelect, NHTable } from "@/components";
+import Icons from "@/constants/icons";
 import { usePatientRecordAccess } from "@/hook/Doctor/PatientRecordAccess";
 
 export const PatientRecordAccess = () => {
-    const { data, handleSelectChange, columns, staticData, filter } = usePatientRecordAccess()
+  const { data, handleSelectChange, columns, staticData, filter } =
+    usePatientRecordAccess();
 
-    return (
-        <NHCard
-            title="Patient Record Access"
-            headerContent={
-                <div className="flex items-center gap-4">
-                    <NHInput
-                        prefix={Icons.SearchIcon}
-                        placeholder="Search Patient"
-                    />
-                    <NHSelect
-                        name="filter"
-                        value={filter}
-                        onChange={(value) => handleSelectChange(value, 'filter')}
-                        placeholder="Select Filter"
-                        options={[
-                            { value: 'day', label: 'Day' },
-                            { value: 'month', label: 'Month' },
-                            { value: 'year', label: 'Year' },
-                        ]}
-                    />
-                </div>
-            }
-        >
-            <NHTable columns={columns} showPagination={true} dataSource={data || staticData} route={"/doctor/patientrecordaccess"} />
-        </NHCard>
-    );
+  return (
+    <NHCard
+      title="Patient Record Access"
+      headerContent={
+        <div className="flex items-center gap-4">
+          <NHInput prefix={Icons.SearchIcon} placeholder="Search Patient" />
+          <NHSelect
+            name="filter"
+            value={filter}
+            onChange={(value) => handleSelectChange(value, "filter")}
+            placeholder="Select Filter"
+            options={[
+              { value: "day", label: "Day" },
+              { value: "month", label: "Month" },
+              { value: "year", label: "Year" },
+            ]}
+          />
+        </div>
+      }
+    >
+      <NHTable
+        columns={columns}
+        showPagination={true}
+        dataSource={data || staticData}
+        route={"/doctor/patientrecordaccess"}
+      />
+    </NHCard>
+  );
 };
