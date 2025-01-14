c818de090a669091899bade40fa76e60cc5abdc3
mohit-swiftrut <mohit.dudhat@swiftrut.com>
2025-01-14 21:34:20 +0530

    Refactor Prescriptions component to manage modal state and improve prescription data handling

--- src/pages/Patients/PersonalHealthRecord/Prescriptions/index.jsx
+++ src/pages/Patients/PersonalHealthRecord/Prescriptions/index.jsx
@@ -1,26 +1,20 @@
-import { AppointmentCard, NHButton, NHCard, NHInput } from "@/components";
+import { AppointmentCard, NHButton, NHCard, NHInput, PrescriptionCard } from "@/components";
 import Icons from "@/constants/icons";
 import { usePatientPrescriptionData } from "@/hook/Patients";
-import { Tag } from "antd";
 import React, { useEffect, useState } from "react";
 
 export const Prescriptions = () => {
   const { loading, data, error } = usePatientPrescriptionData();
   const [prescriptionData, setPrescriptionData] = useState([]);
-
-  useEffect(() => {
-    if (data) {
-      setPrescriptionData(data);
-    }
-  }, [data]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handlePatientDetails = (data) => {
+    setPrescriptionData(data);
+    setIsModalOpen(true);
+  }
   return (
     <>
       <NHCard
-        title={
-          <span className="text-[#030229] text-[26px] font-semibold">
-            Prescriptions
-          </span>
-        }
+        title={<span className='text-[#030229] text-[26px] font-semibold'>Prescriptions</span>}
         headerContent={
           <>
             <div className="me-10">
@@ -30,13 +24,13 @@ export const Prescriptions = () => {
         }
       >
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-          {prescriptionData.map((prescriptions, index) => (
+          {data?.map((prescriptions, index) => (
             <AppointmentCard
               key={prescriptions.prescriptionId}
               headerContent={
                 <>
                   <span
-                    onClick={() => handlePatientDetails()}
+                    onClick={() => handlePatientDetails(prescriptions)}
                     className="cursor-pointer"
                   >
                     {Icons.ViewBillIcon}
@@ -44,11 +38,7 @@ export const Prescriptions = () => {
                 </>
               }
               headerBg={true}
-              title={
-                <span className="font-semibold text-[18px]">
-                  {prescriptions.DoctorName}
-                </span>
-              }
+              title={<span className="font-semibold text-[18px]">{prescriptions.DoctorName}</span>}
               hospitalName={prescriptions.hospitalName}
               diseaseName={prescriptions.DiseaseName}
               date={prescriptions.prescriptionDate}
@@ -56,70 +46,17 @@ export const Prescriptions = () => {
             />
           ))}
         </div>
+        {prescriptionData && (
+          <PrescriptionCard
+            isModalOpen={isModalOpen}
+            onCancel={() => setIsModalOpen(false)}
+            handleClose={() => setIsModalOpen(false)}
+            Title="Prescription"
+            handleOk={() => setIsModalOpen(false)}
+            patientData={prescriptionData}
+          />
+        )}
       </NHCard>
     </>
   );
 };
-import { AppointmentCard, NHButton, NHCard, NHInput, PrescriptionCard } from '@/components';
-import Icons from '@/constants/icons';
-import { usePatientPrescriptionData } from '@/hook/Patients';
-import { Tag } from 'antd';
-import React, { useEffect, useState } from 'react'
-
-export const Prescriptions = () => {
-    const { loading, data, error } = usePatientPrescriptionData();
-    const [prescriptionData, setPrescriptionData] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handlePatientDetails=(data)=>{
-        setPrescriptionData(data);
-        setIsModalOpen(true);
-    }
-    return (
-        <>
-            <NHCard
-                title={<span className='text-[#030229] text-[26px] font-semibold'>Prescriptions</span>}
-                headerContent={
-                    <>
-                        <div className="me-10">
-                            <NHInput prefix={Icons.SearchIcon} placeholder="Search Patient" />
-                        </div>
-                    </>
-                }
-            >
-                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-                    {data?.map((prescriptions, index) => (
-                        <AppointmentCard
-                            key={prescriptions.prescriptionId}
-                            headerContent={
-                                <>
-                                    <span
-                                        onClick={() => handlePatientDetails(prescriptions)}
-                                        className="cursor-pointer"
-                                    >
-                                        {Icons.ViewBillIcon}
-                                    </span>
-                                </>
-                            }
-                            headerBg={true}
-                            title={<span className="font-semibold text-[18px]">{prescriptions.DoctorName}</span>}
-                            hospitalName={prescriptions.hospitalName}
-                            diseaseName={prescriptions.DiseaseName}
-                            date={prescriptions.prescriptionDate}
-                            className="border border-slate-200"
-                        />
-                    ))}
-                </div>
-                {prescriptionData && (
-                        <PrescriptionCard
-                        isModalOpen={isModalOpen}
-                        onCancel={() => setIsModalOpen(false)}
-                        handleClose={() => setIsModalOpen(false)}
-                        Title="Prescription"
-                        handleOk={() => setIsModalOpen(false)}
-                        patientData={prescriptionData}
-                      />
-                      )}
-            </NHCard>
-        </>
-    );
-}
